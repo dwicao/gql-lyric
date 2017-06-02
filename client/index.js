498886a183d84ead2a78762f1d0e931cd66f47b3
dwicao <lutfiandwica@gmail.com>
2017-06-02 21:26:02 +0700

    integrate react router with graphql

--- client/index.js
+++ client/index.js
@@ -7,6 +7,9 @@ import { ApolloProvider } from 'react-apollo';
 import App from './components/App';
 import SongList from './components/SongList';
 import SongCreate from './components/SongCreate';
+import SongDetail from './components/SongDetail';
+
+import './style/style.css';
 
 const client = new ApolloClient({});
 
@@ -17,6 +20,7 @@ const Root = () => {
         <Route path="/" component={App}>
           <IndexRoute component={SongList} />
           <Route path="songs/new" component={SongCreate} />
+          <Route path="songs/:id" component={SongDetail} />
         </Route>
       </Router>
     </ApolloProvider>
